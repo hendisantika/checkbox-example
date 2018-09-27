cc71f4756d3139e3f8b0c1ff2aa8f8a2d9a9240e
Hendi Santika <hendisantika@gmail.com>
2018-09-28 06:38:44 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/checkbox/CheckboxApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.checkbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckboxApplication.class, args);
+    }
+}
